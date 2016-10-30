5df6b6922159857c249a92cb35eb82718290abb9
brjapon <brjapon@psykia.com>
2016-10-30 07:55:49 +0100

    Assigment of channels of maestro and new script that publish the readings of the sharp sensor

--- maestro_subscriber_CW-.py
+++ maestro_subscriber_CW-.py
@@ -11,14 +11,18 @@ from std_msgs.msg import Int32
 #Library of controller Maestro
 import maestro as m
 
+# Maestro channel assignment
+izq = 4
+dcho = 5
+
 # BEGIN CALLBACK
 def callback(msg):
     print "speed= ",msg.data
     # Maestro- (servomotors rotate in the same direction)
     s= m.Controller()
     speed=msg.data
-    s.setTarget(4,-speed)
-    s.setTarget(5,-speed)
+    s.setTarget(izq,-speed)
+    s.setTarget(dcho,-speed)
 # END CALLBACK
 
 rospy.init_node('Maestro_subscriber')
---
+++ sharp_publisher.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# BEGIN ALL
+# BEGIN SHEBANG
+#!/usr/bin/env python
+# END SHEBANG
+
+# Publisher of the distance data of the Sharp sensor of JUS
+
+# BEGIN IMPORT
+import rospy
+#Library of controller Maestro
+import maestro as m
+# END IMPORT
+
+# BEGIN STD_MSGS
+from std_msgs.msg import Int32
+# END STD_MSGS
+
+# Maestro channel assignment
+sharp=0
+
+rospy.init_node('topic_sharp')
+
+# BEGIN PUB
+pub = rospy.Publisher('sharp', Int32)
+# END PUB
+
+# BEGIN LOOP
+s= m.Controller()
+rate = rospy.Rate(2)
+
+while not rospy.is_shutdown():
+	d_min=s.getPosition(sharp)
+	d_max=s.getPosition(sharp)
+	dist=0.5*(d_min+d_max)
+    pub.publish(dist)
+    rate.sleep()
+# END LOOP
+# END ALL
